70a76e3e4e5a2118bfdde2e5425fc60e17523729
nikolaypoh <nikolaypoh@gmail.com>
2016-10-08 16:38:47 +0300

    Revert "Break the build"
    This reverts commit 92db79d9f08cdf218b8ab08f6acbbf67c1ed3ab1.

--- server/todos.js
+++ server/todos.js
@@ -2,4 +2,15 @@ const uuid = require('uuid-v4');
 
 const items = new Map();
 
-module.exports = null;
+module.exports = {
+      add(item) {
+          item.id = uuid();
+          items.set(item.id, item);
+      },
+      remove(id) {
+          items.delete(id);
+      },
+      list() {
+          return Array.from(items.values());
+      }
+};
