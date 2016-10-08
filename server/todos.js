92db79d9f08cdf218b8ab08f6acbbf67c1ed3ab1
nikolaypoh <nikolaypoh@gmail.com>
2016-10-08 15:23:27 +0300

    Break the build

--- server/todos.js
+++ server/todos.js
@@ -2,15 +2,4 @@ const uuid = require('uuid-v4');
 
 const items = new Map();
 
-module.exports = {
-      add(item) {
-          item.id = uuid();
-          items.set(item.id, item);
-      },
-      remove(id) {
-          items.delete(id);
-      },
-      list() {
-          return Array.from(items.values());
-      }
-};
+module.exports = null;
